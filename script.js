b55dddca43e3a1d19a9c46a57c77047feef10648
Anna Chirapongse <achirapongse@gmail.com>
2020-09-28 19:58:59 -0400

    Reverted the html back to orginal

--- script.js
+++ script.js
@@ -1,34 +1,10 @@
 
 var textArea = $("#scheduler").children("tbody").children();
-rows.on("click", function(){
-    console.log(this)
+row.on("click", function(){
+    console.log(this);
     var description = $(this).find(".description");
-    console.log(description)
-})
+    console.log(description);
+});
 
-//     $(document).on("click", function(){
-//         var workScheduler = document.getElementById("jumbotron");
-//         var days = [
-//             "Monday",
-//             "Tuesday",
-//             "Wednesday",
-//             "Thursday",
-//             "Friday",
-//         ];
-//         var times = "";
-//         if (times !== true){
-//           var times = document.createElement("p");
-//         }
-//         if (times !== false)
 
-//         days.forEach(function(create){
-//             var days = document.createElement("div");
-//             newDaysDiv.textContent = event;
-//             days.appendChild(newDaysDiv);
-//         });
-//     });
-
-//    function init(){
-//      var storeDates = JSON.parse(localStorage.getItem("days"));
-//    }
     
