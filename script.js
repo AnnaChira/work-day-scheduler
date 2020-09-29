529357f125605b32f044ccdc31891201ea0abb21
Anna Chirapongse <achirapongse@gmail.com>
2020-09-28 21:33:43 -0400

    Last updates for js

--- script.js
+++ script.js
@@ -3,8 +3,23 @@ var textArea = $("#scheduler").children("tbody").children();
 row.on("click", function(){
     console.log(this);
     var description = $(this).find(".description");
-    console.log(description);
+    var description = document.getElementById("description");
+    addEvent(description, 'imput', updateDescription);
+
+    //console.log(description);
 });
+
+var updateDescription = "<p> imput </p>";
+updateDescription.innerHTML = imput;
+
+var saveBtn = $("#button").bind("click", function(){
+    storeDates.addEventlister 
+});
+
+function init(){
+    var storeDates = JSON.parse(localStorage.getItem("description"));
+}
+
 
 
     
